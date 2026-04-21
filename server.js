00703a799fbcc2c34a3d4df52448a3d31697e10b
Vipertricks <hodactoan181905@gmail.com>
2026-04-21 20:57:14 +0700

    login, register jwt

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const app = express();
 
 // middleware
@@ -11,9 +13,9 @@ app.use(express.json());
 const productRoutes = require("./routes/product.routes");
 const cartRoutes = require("./routes/cart.routes");
 const orderRoutes = require("./routes/order.routes");
-const userRoutes = require("./routes/user.routes");
+const authRoutes = require("./routes/auth.routes");
 
-app.use("/users", userRoutes);
+app.use("/users", authRoutes);
 app.use("/products", productRoutes);
 app.use("/cart", cartRoutes);
 app.use("/orders", orderRoutes);
